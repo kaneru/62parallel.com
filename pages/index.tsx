b779300e71b3749e6a2b0f515cea4dcdd7f07a2e
kaneru <kaneru@fastmail.com>
2022-12-16 16:23:02 +0600

    placeholder WIP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
+import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.container}>
-      <div>
-        <h1 className={styles.logo}>
-          Юность <br /> Севера
-        </h1>
-        <p className={styles.text}>Coming sooon...</p>
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Юность Севера</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.wrapper}>
+          <Image
+            src="/images/placeholder.jpg"
+            alt=""
+            width={360}
+            height={225}
+            className={styles.image}
+          />
+          <p className={styles.text}>Coming sooon...</p>
+        </div>
+      </main>
+    </>
   );
 }
